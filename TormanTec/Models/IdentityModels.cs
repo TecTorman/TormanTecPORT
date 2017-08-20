d4f3975b892f7e5f5e7c4f39b868f4459e549216
TecTorman <tormantec@outlook.com>
2017-08-20 18:46:27 -0300

    update

--- TormanTec/Models/IdentityModels.cs
+++ TormanTec/Models/IdentityModels.cs
@@ -35,7 +35,5 @@ namespace TormanTec.Models
         public System.Data.Entity.DbSet<TormanDomain.Contato> Contatoes { get; set; }
 
         public System.Data.Entity.DbSet<TormanDomain.Projetos> Projetos { get; set; }
-
-        public System.Data.Entity.DbSet<TormanDomain.Email> Emails { get; set; }
     }
 }
